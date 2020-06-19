9edf2189b20f2da33c20322c641962a5b0a14cde
Kate Skorija <kateskorija@gmail.com>
2020-06-19 10:59:41 -0500

    add UI logic to display cost of pizza based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
-function Pizza(size, toppings,) {
+// Business Logic -----------------------
+
+function Pizza(size, toppings, cost) {
   this.size = size,
-  this.toppings = toppings
+  this.toppings = toppings,
+  this.cost = cost
 }
 
 Pizza.prototype.sizeCost = function() {
-  let cost = 4;
+  let sizeCost = 4;
   if (this.size === "medium") {
-    cost += 4
+    cost += 4;
   } else if (this.size === "large") {
-    cost += 6
+    cost += 6;
   }
-  return cost;
+  return sizeCost;
 }
   
+
+// User Interface Logic ------------------
+
+$(document).ready(function() {
+  let userPizza = new Pizza(userSize);
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    const userSize = $("#userSize").val();
+    console.log(userSize);
+
+    userPizza.cost = userPizza.sizeCost(userSize)
+
+    $("#displayTotal").show();
+    $(".userTotal").text("$" + userPizza.cost + ".00")
+
+  });
+})
