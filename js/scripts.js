80c76ff4e934fdefe987a41320c5f7d95efcb6c6
Kate Skorija <kateskorija@gmail.com>
2020-06-19 11:51:50 -0500

    create Order constructor with add and delete pizza prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,42 @@
-// Business Logic -----------------------
+// Business Logic for Order -----------------------
 
-function Pizza(size, toppings, cost) {
+function Order() {
+  this.pizzas = [];
+  this.currentId = 0;
+}
+
+Order.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+
+Order.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+
+Order.prototype.deletePizza = function(id) {
+  for (let index = 0; index < this.pizzas.length; index++) {
+    if (this.pizzas[index]) {
+      if (this.pizzas[index].id == id) {
+        delete this.pizzas[index];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Pizzas ------------------------
+function Pizza(size, toppings, cost, id) {
   this.size = size,
   this.toppings = toppings,
   this.cost = cost
+  this.id = id
+}
+
+Pizza.prototype.fullPizza = function() {
+  return this.size + " pizza with" + this.toppings;
 }
 
 Pizza.prototype.sizeCost = function() {
@@ -19,22 +52,33 @@ Pizza.prototype.sizeCost = function() {
 // User Interface Logic ------------------
 
 $(document).ready(function() {
+  let userOrder = new Order();
   let userPizza = new Pizza();
-  $("form").submit(function(event) {
+  userOrder.addPizza(userPizza);
+
+  let userToppings = [];
+  userPizza.size = userSize
+  userPizza.toppings = userToppings
+  userPizza.cost = userPizza.sizeCost(userSize)
+
+  $("#checkout").click(function(event) {                           // UI for Checkout Button
     event.preventDefault();
     
     const userSize = $("#userSize").val();
-    let userToppings = [];
     $("input:checkbox[name=toppings]:checked").each(function() {
       userToppings.push($(this).val());
     });
-
-    userPizza.size = userSize
-    userPizza.toppings = userToppings
-    userPizza.cost = userPizza.sizeCost(userSize)
 
     $("#displayTotal").show();
     $(".userTotal").text("$" + userPizza.cost + ".00")
     console.log(userPizza);
   });
-})
+  $("#anotherPizza").click(function(event) {                      // UI for Add Another Pizza Button
+    event.preventDefault();
+
+
+
+    $("#displayTotal").show();
+    $(".userTotal").text("$" + userPizza.cost + ".00")
+  });
+}
